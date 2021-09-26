67e9500171f694c8c3e4fc8f0bbbdffbfb06ee6b
abrn <im@him.is>
2021-09-26 04:06:30 +0100

    [create-null] update null name account creator bot for reference usage only
    update create_null and enforce flake8

--- create_null.py
+++ create_null.py
@@ -1,12 +1,21 @@
+"""
+-- THIS METHOD IS PATCHED NOW --
+It worked via spamming account register requests and omitting the username
+field. Every 24 hours a null account would become available and a lucky person
+running a script could register it.
+"""
 import time
 import requests
 import sys
-import random
-# was gonna add multithreading, but knew it was gonna be patched quickly anyway
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from random import randint
 
-email = 'test3423252453@gmail.com'
-password = 'yournewnull'
+# replace this email and password with the one to register the null account
+REGISTER_EMAIL = 'replace@gmail.com'
+REGISTER_PASS = 'yourpass'
+
+# the SOCKS type to use (4 or 5), keep at 4 unless you have issues
+SOCKS_VERSION = 4
+
 
 # takes a --proxyfile arg or just scrapes some from proxyscape
 if '--proxyfile' in sys.argv:
@@ -15,27 +24,27 @@ if '--proxyfile' in sys.argv:
 else:
     proxyReq = requests.get(
         'https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=3000&country=all&ssl=all&anonymity=all')
-
     if proxyReq.status_code == 200:
         proxyList = proxyReq.text.splitlines()
     else:
         print('Failed to retreive proxies via URL.. please load from file')
-        sys.exit(0)
-
+        sys.exit(-1)
 
 
 def make_req():
     if len(proxyList) == 0:
-        return 'No proxies remaining'
+        return 'No available stable proxies remaining.. Try run the script again with the accounts it missed before'
+        sys.exit(-1)
     proxy = get_proxy(proxyList)
 
     req = None
-    while req == None:
+    while req is not None:
+        url = 'https://www.realmofthemadgod.com/account/register?g={}'.format(REGISTER_EMAIL)
         try:
-            req = requests.post('https://www.realmofthemadgod.com/account/register?g={}'.format(email), data={
-                'newGUID': email,
-                'newPassword': password,
-                'gameClientVersion': '1.0.3.0',
+            req = requests.post(url, data={
+                'newGUID': REGISTER_EMAIL,
+                'newPassword': REGISTER_PASS,
+                'gameClientVersion': '2.0.3.1.0',
                 'ignore': '123456',
                 'entrytag': '',
                 'name': '',
@@ -44,7 +53,7 @@ def make_req():
             }, proxies=proxy)
             return req.text
         except Exception as e:
-            print('\nProxy failed, switching')
+            print('\nProxy failed... switching')
             print(e)
             if len(proxyList) == 0:
                 return 'No proxies remaining'
@@ -54,36 +63,40 @@ def make_req():
 
 def get_proxy(fromList):
     proxyCount = len(fromList)
-    proxyIndex = random.randint(0, proxyCount - 1)
+    proxyIndex = randint(0, proxyCount - 1)
     newProxy = fromList[proxyIndex].split(':')
     proxy = {
-        'http': 'socks4://{}:{}'.format(newProxy[0], newProxy[1]),
-        'https': 'socks4://{}:{}'.format(newProxy[0], newProxy[1])
+        'http': f'socks{SOCKS_VERSION}://{newProxy[0]}:{newProxy[1]}',
+        'https': f'socks{SOCKS_VERSION}://{newProxy[0]}:{newProxy[1]}'
     }
     return proxy
 
-tryCount = 0
-result = None
 
-while result is None:
+def main():
+    try_count = 0
+    result = None
 
-    req = make_req()
-    tryCount = tryCount + 1
+    while result is None:
+        req = make_req()
+        try_count += 1
 
-    if req == 'No proxies remaining':
-        print('\n' + req)
-        sys.exit(0)
+        if req == 'No proxies remaining':
+            print('\n' + req)
+            sys.exit(0)
+        if req == '<Error>Error.nameAlreadyInUse</Error>':
+            print('Failed to create, attempt: {}'.format(try_count))
+        elif req[0:6] == '<Error>':
+            print('Failed to create with error: {} - attempt: {}'.format(req, try_count))
+        elif req is None:
+            print('Failed to create, connection failed - attempt: {}'.format(try_count))
+        elif req[0:8] == '<Success>':
+            print(
+                'SUCCESS at time {} with attempt {}\n\nEmail: {}\nPassword: {}'.format(time.time(), try_count, REGISTER_EMAIL, REGISTER_PASS))
+            result = True
+            break
+        else:
+            print('Failed to create with error: {} - attempt: {}'.format(req, try_count))
 
-    if req == '<Error>Error.nameAlreadyInUse</Error>':
-        print('Failed to create, attempt: {}'.format(tryCount))
-    elif req[0:6] == '<Error>':
-        print('Failed to create with error: {} - attempt: {}'.format(req, tryCount))
-    elif req is None:
-        print('Failed to create, connection failed - attempt: {}'.format(tryCount))
-    elif req[0:8] == '<Success>':
-        print(
-            'SUCCESS at time {} with attempt {}\n\nEmail: {}\nPassword: {}'.format(time.time(), tryCount, email, password))
-        result = True
-        break
-    else:
-        print('Failed to create with error: {} - attempt: {}'.format(req, tryCount))
+
+if __name__ == "__main__":
+    main()
